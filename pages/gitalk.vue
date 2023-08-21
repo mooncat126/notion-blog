c4a4c74f23da5ec2b2243fbb9f99e0c2309eb2aa
mooncat126 <xiangyuforwork@gmail.com>
2023-08-21 15:28:34 +0900

    add: gittalk comment function

---
+++ pages/gitalk.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="wrapper-small dark:text-gray-200">
+    <hr />
+    <div class="dark:text-gray-200" id="gitalk-container"></div>
+  </div>
+</template>
+
+<script>
+import "gitalk/dist/gitalk.css";
+import Gitalk from "gitalk";
+
+export default {
+  name: "Gitalk",
+  props: {
+    postId: {
+      type: String,
+      default: "",
+    },
+    postTitle: {
+      type: String,
+      default: "",
+    },
+    githubUsername: {
+      type: String,
+      default: "",
+    },
+    commentClientId: {
+      type: String,
+      default: "",
+    },
+    commentClientSecrets: {
+      type: String,
+      default: "",
+    },
+    commentRepo: {
+      type: String,
+      default: "",
+    },
+  },
+  mounted() {
+    const gitalk = new Gitalk({
+      clientID: this.commentClientId,
+      clientSecret: this.commentClientSecrets,
+      repo: this.commentRepo,
+      owner: this.githubUsername,
+      admin: [this.githubUsername],
+      id: this.postId,
+      title: this.postTitle,
+      distractionFreeMode: false,
+    });
+
+    gitalk.render("gitalk-container");
+  },
+};
+</script>
+<style>
+.gt-btn-text {
+  color: #333 !important;
+}
+.gt-action span {
+  color: #6190e8 !important;
+}
+.gt-svg {
+  @apply dark:text-white;
+}
+.gt-header-textarea {
+  color: #333 !important;
+}
+.markdown-body > * {
+  color: #333 !important;
+}
+</style>
--- pages/posts/_slug.vue
+++ pages/posts/_slug.vue
@@ -8,18 +8,41 @@ import "prismjs/components/prism-python";
 import "prismjs/components/prism-yaml";
 import "prismjs/components/prism-graphql";
 import "prismjs/components/prism-javascript";
+import Gitalk from "../gitalk.vue";
 
 export default {
-  async asyncData({ $notion, params, error, $config: { notionTableId } }) {
+  components: {
+    Gitalk,
+  },
+  async asyncData({
+    $notion,
+    params,
+    error,
+    $config: {
+      notionTableId,
+      githubUsername,
+      commentClientId,
+      commentClientSecrets,
+      commentRepo,
+    },
+  }) {
     const pageTable = await $notion.getPageTable(notionTableId);
     const page = pageTable.find(
-      (item) => item.public && item.slug === params.slug
+      (item) => item?.public && item?.slug === params?.slug
     );
-    const blockMap = await $notion.getPageBlocks(page ? page.id : params.slug);
+    const blockMap = await $notion.getPageBlocks(page?.id);
     if (!blockMap || blockMap.error) {
-      return error({ statusCode: 404, message: "Post not found" });
+      return error({ statusCode: 404, message: blockMap.error });
     }
-    return { blockMap, page };
+
+    return {
+      blockMap,
+      page,
+      githubUsername,
+      commentClientId,
+      commentClientSecrets,
+      commentRepo,
+    };
   },
   data() {
     return {
@@ -30,15 +53,24 @@ export default {
     const post = this.page;
     const title = post?.title;
     const description = post?.description || process.env.DEV_DESCRIPTION;
-    const image = post?.thumbnail[0].url || null;
+    const image = post?.thumbnail[0]?.url || null;
     const atl = post?.atl || "";
-    const tags = post.tags || title;
-    const href = process.env.BASE_URL + `/posts/${post.slug}`;
+    const tags = post?.tags || title;
+    const categories = post?.categories || title;
+    const href = process.env.BASE_URL + `/posts/${post?.slug}`;
     const meta = this.$prepareMeta(
-      { title, description, image, atl, keywords: `${tags}`, url: href },
+      {
+        title,
+        description,
+        image,
+        atl,
+        keywords: `${tags}, ${categories}`,
+        url: href,
+      },
       [{ name: "article:published-time", content: post?.created_at || null }]
     );
     return {
+      post,
       title,
       link: [{ rel: "canonical", href }],
       meta,
@@ -46,15 +78,6 @@ export default {
   },
 };
 </script>
-
-<template>
-  <NotionRenderer
-    :block-map="blockMap"
-    :page-link-options="pageLinkOptions"
-    full-page
-    prism
-  />
-</template>
 
 <style>
 @import "vue-notion/src/styles.css";
@@ -70,7 +93,23 @@ h4,
 span {
   @apply dark:text-white;
 }
-.notion-link {
-  @apply dark:hover:bg-red-500;
-}
 </style>
+
+<template>
+  <div class="wrapper-small">
+    <NotionRenderer
+      :block-map="blockMap"
+      :page-link-options="pageLinkOptions"
+      full-page
+      prism
+    />
+    <Gitalk
+      :post-id="page?.slug"
+      :post-title="page?.title"
+      :github-username="githubUsername"
+      :comment-client-id="commentClientId"
+      :comment-client-secrets="commentClientSecrets"
+      :comment-repo="commentRepo"
+    />
+  </div>
+</template>
